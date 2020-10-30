9203cc953030cd1f94e715e7d2236eb68c8432d6
m1parmar <maparmar368@gmail.com>
2020-10-30 11:36:07 -0400

    This is a Physics ISU Project

---
+++ src/physics/Package/data.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physics.Package;
+
+/**
+ *
+ * @author Mahit
+ */
+public class data {
+    public static String kinOne = "1";
+    public static String kinTwo = "160";
+    public static String kinThree = "5";
+    
+    public static String forceOne = "10";
+    public static String forceTwo = "2.8014 x 10^-7";
+    public static String forceThree = "18";
+    
+    public static String electricityOne = "9.5";
+    public static String electricityTwo = "18";
+    public static String electricityThree = "2.4";
+    
+    public static String energyOne = "25";
+    public static String energyTwo = "61";
+    public static String energyThree = "436";
+    
+    public static String wavesOne = "30";
+    public static String wavesTwo = "24";
+    public static String wavesThree = "1680";
+    
+}
